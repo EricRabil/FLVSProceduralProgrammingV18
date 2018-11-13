450b208d9bae2064f7875a91e4e0039a904d2bba
Eric Rabil <ericjrabil@gmail.com>
2018-11-12 22:16:30 -0500

    1.02 !

---
+++ 1.02/Algorithms.py
@@ -0,0 +1,7 @@
+name = raw_input("Hello! What's your name? ")
+
+print "Hi, " + name + "!"
+
+print "Computational thinking consists of algorithms and breaking down data into its components"
+
+print "Bye, " + name + "!"
