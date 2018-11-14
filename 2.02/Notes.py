f386de418d35687a6596acec040fdc3424da3054
Eric Rabil <ericjrabil@gmail.com>
2018-11-14 09:26:52 -0500

    Notes

---
+++ 2.02/Notes.py
@@ -0,0 +1,13 @@
+def main():
+
+  myNumbers = [3,33,5,2.2,8]
+  print(min(myNumbers))
+
+main()
+
+def main():
+  
+  customerAddress = input("Enter your shipping address.")
+  print(customerAddress)
+
+main()
